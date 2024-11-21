8ea79d54f8b5e65317f281ddbaf4065e19dbcb74
Kulik Pavel <pashakulik58@gmail.com>
2024-11-21 16:30:27 +0700

    It's great

--- calc_edp.c
+++ calc_edp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int read_file(const char *filename, size_t size, int graph[size][size])
 {
@@ -9,7 +10,10 @@ int read_file(const char *filename, size_t size, int graph[size][size])
 
     for (size_t i = 0; i < size; i++)
         for (size_t j = 0; j < size; j++)
-            fscanf(file, "%d", &graph[i][j]);
+        {
+            if (fscanf(file, "%d", &graph[i][j]) == -1)
+                return 1;
+        }
     return 0;
 }
 
@@ -45,7 +49,7 @@ size_t count_pairs(size_t size, int graph[size][size])
     return amount;
 }
 
-size_t get_n(size_t n, size_t size, int graph[size][size])
+size_t get_edp(size_t n, size_t size, int graph[size][size])
 {
     if (n == 0)
         return count_pairs(size, graph);
@@ -57,7 +61,7 @@ size_t get_n(size_t n, size_t size, int graph[size][size])
             {
                 graph[i][j] = 0;
                 graph[j][i] = 0;
-                amount += get_n(n - 1, size, graph);
+                amount += get_edp(n - 1, size, graph);
                 graph[i][j] = n;
                 graph[j][i] = n;
             }
@@ -84,20 +88,20 @@ size_t count_edges(size_t size, int graph[size][size])
 
 int main(int argc, char *argv[])
 {
+    clock_t start = clock();
     if (argc != 3)
     {
         fprintf(stderr, "Необходимо указать размер графа и имя файла\n");
+        fprintf(stderr, "Usage: calc_edp <n_vertex> <filename>\n");
         return 1;
     }
+
     size_t size = atoi(argv[1]);
     int graph[size][size];
-    read_file(argv[2], size, graph);
-
-    for (size_t i = 0; i < size; i++)
+    if (read_file(argv[2], size, graph))
     {
-        for (size_t j = 0; j < size; j++)
-            printf("%d ", graph[i][j]);
-        printf("\n");
+        fprintf(stderr, "Ошибка чтения файла\n");
+        return 1;
     }
 
     size_t n_edges = count_edges(size, graph);
@@ -106,11 +110,26 @@ int main(int argc, char *argv[])
 
     for (size_t i = 0; i < n_edges; i++)
     {
-        ks[i] = get_n(i, size, graph);
+        ks[i] = get_edp(i, size, graph);
         for (size_t n = 0; n < size; n++)
             for (size_t m = 0; m < size; m++)
                 if (graph[n][m] == 2)
                     graph[n][m] = 1;
-        printf("%ld = %ld\n", i, ks[i]);
     }
+    double t = (double)(clock() - start) / CLOCKS_PER_SEC;
+
+    printf("EDP был вычислен за %f секунды\n", t);
+
+    int output_flag = 0;
+
+    for (size_t i = 0; i < n_edges; i++)
+    {
+        if (ks[i] == 0)
+            continue;
+        if (i != 0 && ks[i] > 0 && output_flag != 0)
+            printf(" + ");
+        printf("%ldp^%ldq^%ld", ks[i], n_edges - i - 1, i);
+        output_flag = 1;
+    }
+    printf("\n");
 }
